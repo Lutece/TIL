23c3733db9bb098fa7e9910d9bd1e87258172fc6
Lutece <lutece.me@gmail.com>
2021-12-15 12:16:18 +0900

    typescript chapter1

--- framer/example-framer-next/components/UpDown.tsx
+++ framer/example-framer-next/components/UpDown.tsx
@@ -1,66 +1,80 @@
-import styled from '@emotion/styled';
+import styled from "@emotion/styled";
 import { motion } from "framer-motion";
 import { useState } from "react";
 
 const variants = {
-    up: { y: '100px' },
-    down: { y: '0px'}
+  up: { y: "100px" },
+  down: { y: "0px" },
 };
 
 const variantsPassword = {
-    up: { opacity: 1 },
-    down: { opacity: 0 }
-}
+  up: { opacity: 1 },
+  down: { opacity: 0 },
+};
 
 const StyledInput = styled(motion.input)`
-    position: relative;
-    z-index: 1;
-    border: 0;
-    border-bottom: 2px solid #FCD429;
-    &:hover, &:active, &:focus {
-        outline: 0;
-    }
+  position: relative;
+  z-index: 1;
+  border: 0;
+  border-bottom: 2px solid #fcd429;
+  &:hover,
+  &:active,
+  &:focus {
+    outline: 0;
+  }
+  width: 333px;
+  height: 51px;
+  height: 30px;
+  background-color: transparent;
 `;
 
 const StyledInputPassword = styled(motion.input)`
-    position: absolute;
-    left: 0;
-    top: 0;
-    border: 0;
-    border-bottom: 2px solid #FCD429;
-    &:hover, &:active, &:focus {
-        outline: 0;
-    }
+  position: absolute;
+  left: 0;
+  top: 0;
+  border: 0;
+  width: 333px;
+  height: 30px;
+  border-bottom: 2px solid #fcd429;
+  &:hover,
+  &:active,
+  &:focus {
+    outline: 0;
+  }
+  font-size: 18px;
+  background-color: transparent;
 `;
 
 const UpDown = () => {
+  const [isUp, setIsUp] = useState(false);
 
-    const [isUp, setIsUp] = useState(false);
-
-    return(
-        <div style={{ position: 'relative' }}>
-            <StyledInputPassword
-                animate={isUp ? 'up' : 'down'}
-                variants={variantsPassword}
-                transition={{duration: 2, ease: 'backOut'}}
-                onKeyPress={(e) => {
-                    if(e.key === 'Enter') {
-                        setIsUp(true);
-                    }
-                }}
-            />
-            <StyledInput
-                animate={isUp ? 'up' : 'down'}
-                variants={variants}
-                transition={{duration: 0.6, ease: 'backOut'}}
-                onKeyPress={(e) => {
-                    if(e.key === 'Enter') {
-                        setIsUp(true);
-                    }
-                }}
-            />
-        </div>
-    )
+  return (
+    <div
+      style={{ position: "relative", height: "500px", backgroundColor: "#fff" }}
+    >
+      <StyledInputPassword
+        animate={isUp ? "up" : "down"}
+        variants={variantsPassword}
+        transition={{ duration: 2, ease: "backOut" }}
+        onKeyPress={(e) => {
+          if (e.key === "Enter") {
+            setIsUp(true);
+          }
+        }}
+        type={"password"}
+      />
+      <StyledInput
+        animate={isUp ? "up" : "down"}
+        variants={variants}
+        transition={{ duration: 0.6, ease: "backOut" }}
+        onKeyPress={(e) => {
+          if (e.key === "Enter") {
+            setIsUp(true);
+          }
+        }}
+      />
+    </div>
+  );
 };
 
 export default UpDown;
