70828989d3da34ba26dc99be6c314fa6004e9aad
Lutece <lutece.me@gmail.com>
2021-12-16 16:39:18 +0900

    typescript chapter 2

---
+++ typescript/books/typescript_programming/examples/chapter-2/dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+console.log("hello world");
+let a = 1 + 2;
+let b = a + 3;
+let c = {
+    apple: a,
+    banana: b,
+};
+let d = c.apple * 4;
+console.log(d);
+//# sourceMappingURL=index.js.map
---
+++ typescript/books/typescript_programming/examples/chapter-2/src/index.ts
@@ -0,0 +1,10 @@
+console.log("hello world");
+
+let a = 1 + 2;
+let b = a + 3;
+let c = {
+  apple: a,
+  banana: b,
+};
+let d = c.apple * 4;
+console.log(d);
